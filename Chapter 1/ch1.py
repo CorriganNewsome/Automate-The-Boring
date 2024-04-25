7b545011abce13d4cc4caf6d5989b08a71577337
Corrigan <corrigannewsome@gmail.com>
2024-04-25 10:33:24 -0600

    added ch2 folder and file

--- Chapter 1/ch1.py
+++ Chapter 1/ch1.py
@@ -1,3 +1,9 @@
-spam = 'spam'
-spam + spamspam
-spam * 3
+print('Hello World')
+print('What is your name?')
+myName = input()
+print('It is nice to meet you,' + myName)
+print('The length of your name is')
+print(len(myName))
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year')
